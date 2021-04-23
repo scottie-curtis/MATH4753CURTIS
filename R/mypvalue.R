604c6100cb7ee39e08b981eb0ff4925540901b26
Scottie Curtis <scottie.a.curtis@gmail.com>
2021-04-23 06:07:27 -0500

    Added mypvalue() from Lab 12

---
+++ R/mypvalue.R
@@ -0,0 +1,46 @@
+#' @title function for p-values
+#'
+#' @param t0 a calculated value of t
+#' @param xmax the maximum value of x to be plotted
+#' @param n the sample size
+#' @param alpha the value of alpha, e.g. 1-confidence level
+#'
+#' @return a plot with p-value areas, quantiles, and a p-value
+#' @export
+#'
+#' @examples
+#' \dontrun mypvalue(t0=2.3563,n=30,alpha=0.05)
+mypvalue=function(t0,xmax=4,n=20, alpha=0.05){
+  #calculate alpha/2
+  va=round(pt(-t0,df=n-1),4)
+  pv=2*va
+
+  # plot the t dist
+  curve(dt(x,df=n-1),xlim=c(-xmax,xmax),ylab="T Density",xlab=expression(t),
+        main=substitute(paste("P-value=", pv, " alpha=", alpha)))
+
+
+  # set up points on the polygon to the right
+  xcurve=seq(t0,xmax,length=1000)
+  ycurve=dt(xcurve,df=n-1)
+
+  # set up points to the left
+  xlcurve=seq(-t0,-xmax,length=1000)
+  ylcurve=dt(xcurve,df=n-1)
+
+  # Shade in the polygon defined by the line segments
+  polygon(c(t0,xcurve,xmax),c(0,ycurve,0),col="green")
+  polygon(c(-t0,xlcurve,-xmax),c(0,ylcurve,0),col="green")
+
+  # make quantiles
+  q=qt(1-alpha/2,n-1)
+  abline( v=c(q,-q),lwd=2) # plot the cut off t value
+  axis(3,c(q,-q),c(expression(abs(t[alpha/2])),expression(-abs(t[alpha/2]))))
+
+
+  # Annotation
+  text(0.5*(t0+xmax),max(ycurve),substitute(paste(area, "=",va)))
+  text(-0.5*(t0+xmax),max(ycurve),expression(area))
+
+  return(list(q=q,pvalue=pv))
+}
